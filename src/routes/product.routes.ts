8f4578bc0f49f65a9fef2e83d662752ddec3a2f4
Steve <52472399+steveneec@users.noreply.github.com>
2022-09-02 00:26:03 -0500

    Update product.routes.ts

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -6,7 +6,7 @@ const router = Router();
 
 router.get("/", productController.getLastProducts);
 router.get("/store/:store", productController.getProductsByStore);
-router.get("/category/:category", productController.getProductsByCategory);
+router.get("/category/cat/:category", productController.getProductsByCategory);
 router.post("/", [uploadProduct, productController.newProduct]);
 router.post("/multiple/many/get", productController.getManyProducts);
 
